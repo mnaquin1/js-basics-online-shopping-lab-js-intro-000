8b416afb9499a68cea3d509f24dc5ff7c4571a20
Michael Naquin <mnaquin1@yahoo.com>
2020-09-10 17:42:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,5 +62,7 @@ function placeOrder(cardNumber) {
 if (!cardNumber){
   return 'Sorry, we don\'t have a credit card on file for you.'
 }
+cart.splice(0,cart.length)
 return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+
 }
